a952eece5d3a59aebda59b019b52f3f464a20196
mayowaibi <isaac.ibidun@gmail.com>
2025-11-17 16:48:42 -0500

    WXT + React Initialization

---
+++ wxt.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, type WxtViteConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+	srcDir: "src",
+	outDir: "dist",
+	modules: ["@wxt-dev/module-react"],
+	vite: () =>
+		({
+			plugins: [tailwindcss()],
+		} as WxtViteConfig),
+});
